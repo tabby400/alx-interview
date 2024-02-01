c17169148b4d46b0b48cac3b919a45ec9b65c6d3
tabby400 <tabbymargaret@gmail.com>
2024-02-01 11:29:32 +0000

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,13 +13,13 @@ def validUTF8(data):
     track_bytes = 0  # keep track of bytes rep a character
     for byte in data:  # iterate every byte in the data list
         if track_bytes == 0:  # if it is 0 shows start of a character
-            if (byte >> 7) == 0b0:  # a 2 byte char
+            if (byte >> 7) == 0b0:  # a 1 byte char
                 track_bytes = 0
-            elif (byte >> 5) == 0b110:  # a 3 byte char
+            elif (byte >> 5) == 0b110:  # a 2 byte char
                 track_bytes = 1
-            elif (byte >> 4) == 0b1110:  # a 4 byte char
+            elif (byte >> 4) == 0b1110:  # a 3 byte char
                 track_bytes = 2
-            elif (byte >> 3  == 0b11110):  # most significant bit is where its not suppose
+            elif (byte >> 3 == 0b11110):  # 4 byte char
                 track_bytes = 3
             else:
                 return False
