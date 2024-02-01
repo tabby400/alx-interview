5c1b76f3deee1c7efec3a390a01afc02dba73352
tabby400 <tabbymargaret@gmail.com>
2024-02-01 11:05:46 +0000

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+checking if a given set of data conforms to the rules
+of UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Returns True if data is a valid UTF-8 encoding
+    otherwise False is returned
+    """
+    track_bytes = 0  # keep track of bytes rep a character
+    for byte in data:  # iterate every byte in the data list
+        if track_bytes == 0:  # if it is 0 shows start of a character
+            if (byte >> 5) == 0b110:  # a 2 byte char
+                track_bytes = 2
+            elif (byte >> 4) == 0b1110:  # a 3 byte char
+                track_bytes = 3
+            elif (byte >> 3) == 0b11110:  # a 4 byte char
+                track_bytes = 4
+            elif (byte >> 7):  # most significant bit is where its not suppose
+                return False
+        else:
+            if (byte >> 6) != 0b10:  # checks if it is a continuation byte 10
+                return False
+            track_bytes = track_bytes - 1
+    return track_bytes == 0  # all chars have been validated and encoded utf-8
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,2 @@
+#### UTF-8 Validation
+ while computers can understand simple text using ASCII, UTF-8 encoding allows them to handle a much broader range of characters, making it essential for communication in diverse languages and contexts on the internet and in digital communication
