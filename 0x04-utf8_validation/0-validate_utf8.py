e1269ffc85d995bb279e2a6d48560dc57afdb7ac
tabby400 <tabbymargaret@gmail.com>
2024-02-02 09:51:13 +0000

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,21 +10,19 @@ def validUTF8(data):
     Returns True if data is a valid UTF-8 encoding
     otherwise False is returned
     """
-    track_bytes = 0  # keep track of bytes rep a character
-    for byte in data:  # iterate every byte in the data list
-        if track_bytes == 0:  # if it is 0 shows start of a character
-            if (byte >> 7) == 0b0:  # a 1 byte char
-                track_bytes = 0
-            elif (byte >> 5) == 0b110:  # a 2 byte char
-                track_bytes = 1
-            elif (byte >> 4) == 0b1110:  # a 3 byte char
-                track_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 4 byte char
-                track_bytes = 3
-            else:
-                return False
+    track_bytes = 0  # tracks the remaining bytes to complete a char
+    for byte in data:
+        mask = 1 << 7  # sets up a mask leftmost bit set to 1
+        if not track_bytes:  # if track bytes is 0 start of new char
+            while byte & mask:  # the most significant bit is 1
+                track_bytes += 1
+                mask >>= 1
+            if not track_bytes:  # if no next bit in the byte it continues
+                continue
+            if track_bytes == 1 or track_bytes > 4:
+                return False  # if the number of bytes is valid
         else:
-            if (byte >> 6) != 0b10:  # checks if it is a continuation byte 10
+            if byte >> 6 != 0b10:  # if its a continuation byte
                 return False
-            track_bytes = track_bytes - 1
-    return track_bytes == 0  # all chars have been validated and encoded utf-8
+        track_bytes -= 1
+    return track_bytes == 0
